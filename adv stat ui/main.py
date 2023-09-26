e53646419aaec13551f75b2820dd57e9d191ae1f
Taysir Alam <85037857+taysir-alam@users.noreply.github.com>
2023-09-25 23:29:34 -0500

    ui changes

--- adv stat ui/main.py
+++ adv stat ui/main.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 # Sample data for teams and metrics (replace with your data)
-teams = ["MOHAWK", "MEMORIAL", "CONCORDIA"]
+teams = ["09/23/2023:MOHAWK", "MEMORIAL", "CONCORDIA"]
 metrics = ["ANGLE %", "AWAY %", "SWING %", "TRANSITION %", "FREE PLAY %", "BLOB %", "SLOB %", "CONTEST %", "SOUL %"]
 
 # Sample data for metrics per team (replace with your data)
 team_metrics = {
-    "MOHAWK": [26, 29, 0, 39, 25, 50, 40, 48, 54],
+    "09/23/2023:MOHAWK": [26, 29, 0, 39, 25, 50, 40, 48, 54],
     "MEMORIAL": [0.68, 0.75, 0.60, 0.64, 0.78, 0.70, 0.75, 0.61, 0.66],
     "CONCORDIA": [0.72, 0.79, 0.65, 0.69, 0.82, 0.74, 0.79, 0.65, 0.70],
 }
